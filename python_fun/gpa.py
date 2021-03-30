c655619f7d681fb25f07d4348a4266bbb62a8f18
Jesus Ugarte <Jugarteosta@knights.ucf.edu>
2021-03-30 11:20:33 -0400

    upgrading gpa python

--- python_fun/gpa.py
+++ python_fun/gpa.py
@@ -1,6 +1,11 @@
+#Jesus Ugarte
+#University of central Florida
+#GPA Calculator
+
 def main():
     print("Welcome to the College GPA Calculator\n")
-    nClasses = int(input("How Many Classes are you taking: "))
+    print("How Many Classes are you taking: ")
+    nClasses = scan()
 
     sum = 0
     score = 0
@@ -10,16 +15,30 @@ def main():
         score = score + ((hours/get_score(i))*4)
     print("\n\n## GPA IS: %.2f ##\n" %((sum/score)*4))
 
+def scan():
+    while True:
+        #Try Except code block for exception handling
+        try:
+            #Try the two lines of codes if there is an exception, throw Value error exception
+            N1 = int(input("Please enter Value: "))
+            #If code reaches here, there is no error
+            break
+        except ValueError:
+            #If there is any expeption at any point, run this line of code
+            print("Invalid Value entered.  Try again...")    
+    return N1
+
 def get_score(i):
     print("\n  A    A-   B+   B    B-   C+   C    C-    F")
-    print("(4.0)(3.7)(3.3)(3.0)(2.7)(2.3)(2.0)(1.70)(0.0)")
-    print("")
+    print("(4.0)(3.7)(3.3)(3.0)(2.7)(2.3)(2.0)(1.70)(0.0)\n")
     print("Please enter number in range")
     return value_Check(i)
 
 def get_hours(i):
     print("------------------------------------------------")
-    return float(input("\nHow many credit hours for class #%d: " %(i+1)))
+    print("\nHow many credit hours for class #%d: " %(i+1))
+    hours = scan()
+    return hours
 
 def value_Check(i):
 
