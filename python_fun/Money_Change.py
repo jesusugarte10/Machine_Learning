52543ce7cf26b6ce22df0d8745623ff83731b965
Jesus Ugarte <Jugarteosta@knights.ucf.edu>
2021-03-30 12:35:05 -0400

    Adding credit card Project

--- python_fun/Money_Change.py
+++ python_fun/Money_Change.py
@@ -1,13 +1,29 @@
+#Jesus ugarte
+#University Of Central Florida
+# This program Determines how many coins are neccesary to give exact change
 def main():
-    print("this app counts how many coins are needed")
-    n = float(input("Please enter ammount in $$: "))
+    print("this app counts how many coins are needed given ammount in $")
+    n = scan()
     func(n)
 
+def scan():
+    while True:
+        #Try Except code block for exception handling
+        try:
+            #Try the two lines of codes if there is an exception, throw Value error exception
+            N1 = float(input("Please enter Ammount in $$: "))
+            #If code reaches here, there is no error
+            break
+        except ValueError:
+            #If there is any expeption at any point, run this line of code
+            print("Invalid Value entered.  Try again...")    
+    return N1
+
 def func(n):
-    quarter = 0
-    dime = 0
-    nickel = 0
-    pennie = 0
+    quarter = 0;
+    dime = 0;
+    nickel = 0;
+    penny = 0
 
     while n>0:
         if (n >= 0.25):
@@ -20,10 +36,10 @@ def func(n):
             nickel = nickel + 1
             n = n - 0.05
         else:
-            pennie = pennie + 1
+            penny = penny + 1
             n = n - 0.01
         n = round(n, 2)
 
-    print(f'we need Q:{quarter} D:{dime} N:{nickel} P:{pennie}')
+    print(f'we need Q:{quarter} D:{dime} N:{nickel} P:{penny}')
 
 main()                                                                                                                                                                                               
