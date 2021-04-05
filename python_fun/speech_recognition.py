25ce36b18162a58301618dc45b34700f7eb4dd2f
Jesus Ugarte <Jugarteosta@knights.ucf.edu>
2021-04-04 20:03:01 -0400

    update

--- python_fun/speech_recognition.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition
-
-recognizer = speech_recognition.Recognizer()
-with speech_recognition.Microphone() as source:
-    print("Say somehting: ")
-    audio = recognizer.listen(source)
-
-print("You said:")
-print(recognizer.recognize_google(audio))
